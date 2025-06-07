2ec7405670991af35b6388016c7a8f09095fc7c2
Asiandayboy <russellcheeseday@gmail.com>
2025-06-07 02:32:30 -0400

    Add prometheus integration

--- src/app.py
+++ src/app.py
@@ -1,14 +1,29 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, Response
 from ai_routes import ai_bp
 from uids_routes import uids_bp
 from cache import init_db
 from flasgger import Swagger
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
 
 app = Flask(__name__)
 app.register_blueprint(ai_bp, url_prefix="/api")
 app.register_blueprint(uids_bp, url_prefix="/api")
-
 swagger = Swagger(app)
+
+
+REQUEST_COUNT = Counter("http_requests_total", "Total HTTP Requests", ["method", "endpoint"])
+
+
+@app.before_request
+def before_request():
+    REQUEST_COUNT.labels(method=request.method, endpoint=request.path).inc()
+
+
+
+@app.route("/metrics")
+def metrics():
+    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
+
 
 
 @app.route("/")
@@ -24,6 +39,9 @@ def index():
                 text/html
     """
     return render_template("index.html")
+
+
+
 
 
 
