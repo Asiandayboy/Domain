169f427b2ad529cc49b3bfb47622f8addf97dadb
Asiandayboy <russellcheeseday@gmail.com>
2025-06-07 05:07:59 -0400

    Provide context to AI model

--- src/ai_service.py
+++ src/ai_service.py
@@ -10,7 +10,15 @@ genai.configure(api_key=os.getenv("GG_API_KEY"))
 model = genai.GenerativeModel("gemini-1.5-flash-002")
 
 
+AI_INSTRUCTION = (
+    "You are an expert in embedded systems and IoT."
+    "You provide technical and helpful responses."
+    "Use traffic dataset trends when applicable."
+)
+
 def get_ai_response(prompt):
-    response = model.generate_content(prompt)
+    response = model.generate_content(
+        [AI_INSTRUCTION, prompt]
+    )
     html_response = markdown.markdown(response.text)
     return html_response
